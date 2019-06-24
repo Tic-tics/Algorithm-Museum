b441b50b16c127128fe4129b6ec003da75ac1876
Tic-tics <ajs130430@gmail.com>
2019-06-24 14:55:34 -0500

    Added solutions to 4.5, 4.7, and 4.9.

---
+++ ProgrammingInLua/ch04/4.5.lua
@@ -0,0 +1,10 @@
+function removeSub(original, start, len)
+  if start < 1 then start = 1 end
+  if start + len > #original then len = #original - start end
+  return original:sub(1,start-1) .. original:sub(start+len,-1)
+end
+
+print(removeSub("Hello World", 1, 0))
+print(removeSub("Hello World", 3, 2))
+print(removeSub("Hello World", 7, 4))
+print(removeSub("Hello World", 5, 1))
