86dbed04aa1b2b2dafaf5fb3c29fd193e3410515
Tic-tics <ajs130430@gmail.com>
2019-06-25 22:55:28 -0500

    Create 5.7.lua

---
+++ ProgrammingInLua/ch05/5.7.lua
@@ -0,0 +1,20 @@
+function transport(startList, startIndex, sectionLen, targetList, targetIndex)
+  table.move(targetList, targetIndex, #targetList, targetIndex + sectionLen)
+  table.move(startList, startIndex, startIndex + sectionLen - 1, targetIndex, targetList) 
+end
+
+original = {1,2,3,4,5,6,7,8,9,0}
+target = {1,2,3,4,5,6,7,8,9,0}
+
+print("Before Transport")
+for k,v in ipairs(target) do
+  print(k,v)
+end
+
+
+print("\n\nAfter Transport")
+transport(original, 4, 3, target, 5)
+
+for k,v in ipairs(target) do
+  print(k,v)
+end
