23e0c70491ed3d456b5e81fad487e5aa1b11706a
Tic-tics <ajs130430@gmail.com>
2019-07-01 13:47:20 -0500

    Added 7.1 solution

---
+++ ProgrammingInLua/ch07/7.1.lua
@@ -0,0 +1,45 @@
+function sortFile(inp, out)
+  local inp = inp or nil --signify that it is optional
+  local out = out or inp --only nil if both are nil
+  
+  --initialize input file
+  if inp then
+    inp = assert(io.open(inp, "r"))
+  else
+    inp = io.stdin
+  end
+  
+  --read in the lines
+  local lines = {}
+  for line in inp:lines() do
+    lines[#lines+1]=line
+  end
+  
+  --sort all the lines
+  table.sort(lines)
+  
+  --done with input
+  inp:close()
+  
+  --open output stream
+  if out then
+    out = assert(io.open(out, "w"))
+  else
+    out = io.stdout
+  end
+  
+  --output all the lines
+  for _,v in pairs(lines) do
+    out:write(v, "\n")
+  end
+  
+  --close output stream
+  out:close()
+end
+
+--ZeroBrane is set with the algorithm museum as the project base.
+--Unfortunately, you may need to change this path for the demo to work, depending on your setup.
+sortFile(".\\ProgrammingInLua\\ch07\\7.1.in",".\\ProgrammingInLua\\ch07\\7.1.out")
+
+--This call is destructive to the test file
+--sortFile(".\\ProgrammingInLua\\ch07\\7.1.in")
